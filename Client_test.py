1385270a1eedba34a03a762f62213d449809efa4
BINainier <31227940+BINainier@users.noreply.github.com>
2019-10-31 14:19:31 +0800

    Update Client_test.py

--- Client_test.py
+++ Client_test.py
@@ -3,13 +3,18 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # create connection
 s.connect(('127.0.0.1', 9999))
-# accept welcome message
-print(s.recv(1024).decode('utf-8'))
-s.send(b'0000')
-print(s.recv(1024).decode('utf-8'))
+#send SUPI
+data='xidian'
+#s.send(data.encode('utf-8'))
+s.send(data)
+
+# print 5g HE AV
+while True:
+    print 'accept 5g HE AV from UDM:\n'+s.recv(1024)
+
 #for data in [b'Michael', b'Tracy', b'Sarah']:
 
  #   s.send(data)
-print(s.recv(1024).decode('utf-8'))
+
 #s.send(b'exit')
 #s.close()
