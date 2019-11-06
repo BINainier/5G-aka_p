d1a44e9874633b55ae9456b3dd143f12bccf51c4
BINainier <31227940+BINainier@users.noreply.github.com>
2019-11-06 22:23:13 +0800

    Update UE.py

--- UE.py
+++ UE.py
@@ -1,15 +1,37 @@
 # -*- coding:utf-8 -*-
 #UE receive rand and AUTN from SEAF, check freshness by AUTN
-#Then ues res+ik+ck generate res_star
-#Send res_star to SEAF
+#Then ues res+ik+ck generate res*
+#Send res* to SEAF
 
-import milenage
+
 import binascii
 import hmac
 from hashlib import sha256
 import socket
+import milenage
 import random
 
+def Generate_IMSI():
+    imsi = '46000'
+    for num in range(0,9):
+        imsi=imsi + str(random.choice('0123456789'))
+    return imsi
+
+#generate SUCI
+
+def Generate_SUCI(imsi):
+    mcc = imsi[:3]
+    mnc = imsi[3:5]
+    msin = imsi[5:]
+    suci = '0'+mcc+mnc+'678'+'0'+'0'+msin
+    return suci
+
+#send SUCI to seaf
+def SendTo_SEAF(data, host, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(host, port)
+    print 'send SUCI to SEAF\n'
+    client.send(data)
 
 #resolve AUTN
 def AUTN_resolve(AUTN):
@@ -48,8 +70,9 @@ def KDF_res_star(ck, ik, P0, L0, rand, res):
     L1 = str(len(rand))
     P2 = res
     L2 = str(len(res))
-    s = binascii.hexlify('20' + P0 + L0 + P1 + L1 + P2 + L2)
-    res_star=hmac.new(key, s, digestmod=sha256).digest()
+    s = binascii.hexlify('6B' + P0 + L0 + P1 + L1 + P2 + L2)
+    tmp=hmac.new(key, s, digestmod=sha256).hexdigest()
+    res_star=tmp[32:]
     return res_star
 
 #check MAC
@@ -68,3 +91,9 @@ def generate_auts(sqn_ms, ak_star, xmac_s):
     tmp = milenage.LogicalXOR(sqn_ms, ak_star)
     auts = tmp+xmac_s
     return auts
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
