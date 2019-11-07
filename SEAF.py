4790900debb71c90d6d2caf1ee29be79a9491d00
BINainier <31227940+BINainier@users.noreply.github.com>
2019-11-07 13:18:48 +0800

    Update SEAF.py

--- SEAF.py
+++ SEAF.py
@@ -12,7 +12,6 @@ from hashlib import sha256
 import socket
 import random
 
-
 #generate hres*
 def KDF_hres_star(rand, res_star):
     s = binascii.hexlify(rand+res_star)
@@ -31,4 +30,86 @@ def KDF_Kamf(K_seaf, supi):
     s = binascii.hexlify('6D'+P0+L0+P1+L1)
     K_amf = hmac.new(K_seaf, s, digestmod=sha256).hexdigest()
     return K_amf
+
+def SentTo_UE(data,host,port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    print 'send RAND and ANTN to UE '
+
+def SentTo_AUSF(data,host,port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+def AV_resolve(data):
+    rand = data[:32]
+    autn = data[32:64]
+    hxres_star = data[64:128]
+    K_seaf = data[128:]
+    return rand,autn,hxres_star,K_seaf
+#handle 5gAV and supi from AUSF
+def AUSF_resolve(data):
+    AV=data[:192]
+    supi=data[192:]
+    return AV,supi
+
+
+def main():
+    host = ''  # LOCAL Server IP
+    host2 = '127.0.0.1'  # AUSF Server IP
+    host3 = '127.0.0.1'  # UE Server IP
+    port = 7001  # LOCAL Server Port
+    port2 = 6001  # AUSF Server Port
+    port3 = 9999  # UE ServerPort
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
+    print('Waiting for connection...')
+    print('等待与UE 和 AUSF连接')
+    # listen to port 7001
+    while True:
+        while True:
+            sock, addr = server.accept()
+            print 'got connected from', addr
+            data = sock.recv(1024)
+            length = len(data)
+            ##supi
+            if length < 32:
+                print 'get SUCI and snName from UE:\n'
+                print data
+                print 'send SUCI to AUSF'
+                SentTo_AUSF(data, host2, port2)
+
+            elif length >= 160:
+                print 'get 5gAV and SUPI from AUSF'
+                AV, supi = AUSF_resolve(data)
+                global hxres_star, K_seaf, rand  # save these three parameters
+                hxres_star = 0
+                K_seaf = 0
+                rand = 0
+                rand, autn, hxres_star, K_seaf = AV_resolve(AV)
+                # sent rand and AUTN to UE
+                message = rand + autn
+                SentTo_UE(message, host3, port3)
+                print 'send rand and autn to ue'
+
+            elif length == 32:
+                print 'get res* from UE'
+                res_star = data
+
+                hres_star = KDF_hres_star(rand, res_star)
+                # check
+                if str(hres_star) == str(hxres_star):
+                    print 'SEAF Authentication successful'
+                    SentTo_AUSF(res_star, host2, port2)
+                else:
+                    print 'SEAF Authentication fail'
+
+            elif length == 48:
+                print 'get auts from UE'
+                print 'Authentication failed'
+
+
+
+if __name__ == "__main__":
+    main()
 
